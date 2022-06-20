8fc6cf52a9f2228e805586906e9038dfe43ef735
joseade <jlavila@intekglobal.com>
2022-06-19 22:19:40 -0700

    add resolvers schema

--- src/schema.js
+++ src/schema.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server');
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   type Query {
@@ -28,6 +28,8 @@ const typeDefs = gql`
 
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
+    "The track's full duration, in seconds"
+    durationInSeconds: Int
     id: ID!
     "The track's title"
     title: String!
@@ -35,8 +37,8 @@ const typeDefs = gql`
     author: Author!
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    "The track's approximate length to complete, in minutes"
-    length: Int
+    "The track's approximate length to complete, in seconds"
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
@@ -58,11 +60,13 @@ const typeDefs = gql`
 
   "A Module is a single unit of teaching. Multiple Modules compose a Track"
   type Module {
+    "The module's video duration, in seconds"
+    durationInSeconds: Int
     id: ID!
     "The module's title"
     title: String!
-    "The module's length in minutes"
-    length: Int
+    "The module's length in seconds"
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
